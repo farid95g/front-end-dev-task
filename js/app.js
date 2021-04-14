ca06292ced719f05a098e61c499f19c6ee60ea9a
farid95g <f95guluzade@gmail.com>
2021-04-15 00:33:02 +0400

    header section animations

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ fetch("https://api.weatherapi.com/v1/current.json?key=98e270f30e9243299f71842272
 
 // adding animation for the load event of document
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("header section").forEach(section => section.style.opacity = "1");
+  document.querySelectorAll("header section").forEach(section => section.style.transform = "translateY(0)");
   document.querySelector(".left-side-msg").style.transform = "translateX(0)";
   document.querySelector(".right-side-msg").style.transform = "translateX(0)";
 })
