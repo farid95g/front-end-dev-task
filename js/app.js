7f1fdb6654568deb533efcf24b00fecd81ad7e9b
farid95g <f95guluzade@gmail.com>
2021-04-15 16:42:51 +0400

    products section

--- js/app.js
+++ js/app.js
@@ -4,9 +4,6 @@ fetch("https://api.weatherapi.com/v1/current.json?key=98e270f30e9243299f71842272
   .then(res => res.json())
   .then(data => currentWeather.querySelector("span").textContent = data.current.temp_c);
 
-// adding animation for the load event of document
-window.addEventListener("load", () => {
-  document.querySelectorAll("header section").forEach(section => section.classList.add("animated"));
-  document.querySelector(".left-side-msg").classList.add("animated");
-  document.querySelector(".right-side-msg").classList.add("animated");
-})
+// dynamic year
+const currentYear = document.querySelector(".current-year");
+currentYear.textContent = ` ${new Date().getFullYear()} `;
